955751a08a48733f10ff8b71d2effc1ba9a809a0
Joel Sutherland <jdsutherland3@gmail.com>
2015-07-10 16:46:49 -0400

    switch to new preferred method for Docker installation

--- owdocker/recipes/install.rb
+++ owdocker/recipes/install.rb
@@ -1,19 +1,5 @@
-include_recipe 'apt'
-package 'apt-transport-https'
-
-apt_repository "docker" do
-  uri "https://get.docker.com/ubuntu"
-  distribution "docker"
-  components ["main"]
-  keyserver "hkp://keyserver.ubuntu.com:80"
-  key "36A1D7869245C8950F966E92D8576A8BA88D21E9"
-  not_if { File.exists?("/etc/apt/sources.list.d/docker.list") }
-end
-
-# Install Docker latest version
-package "docker" do
-  package_name "lxc-docker"
-  action :install
+execute "install docker" do
+  command 'wget -qO- https://get.docker.com/ | sh'
 end
 
 service "docker" do
