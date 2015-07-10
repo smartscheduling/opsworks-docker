78e082816f67bd3752978f726505d637af690d13
Joel Sutherland <jdsutherland3@gmail.com>
2015-07-10 17:32:26 -0400

    support for better storage on Ubuntu 14.04

--- owdocker/recipes/install.rb
+++ owdocker/recipes/install.rb
@@ -1,5 +1,12 @@
-execute "install docker" do
-  command 'wget -qO- https://get.docker.com/ | sh'
+bash "install docker" do
+  code <<-EOF
+    sudo apt-get update
+    sudo apt-get -y install linux-image-extra-$(uname -r)
+    sudo sh -c "wget -qO- https://get.docker.io/gpg | apt-key add -"
+    sudo sh -c "echo deb https://get.docker.io/ubuntu docker main\ > /etc/apt/sources.list.d/docker.list"
+    sudo apt-get update
+    sudo apt-get -y install lxc-docker
+  EOF
 end
 
 service "docker" do
